59308801ed4494b97e46b6f682fceab04c26af3b
82106 <eldms0406@naver.com>
2023-08-12 17:55:58 +0900

    [feat] #3 genderType 구현(남성, 여성, 선택안함)

---
+++ app/src/main/java/com/cecd/exitmed/util/DebugTree.kt
@@ -0,0 +1,8 @@
+package com.cecd.exitmed.util
+
+import timber.log.Timber
+
+class GPDebugTree : Timber.DebugTree() {
+    override fun createStackElementTag(element: StackTraceElement) =
+        "${element.fileName}:${element.lineNumber}#${element.methodName}"
+}
