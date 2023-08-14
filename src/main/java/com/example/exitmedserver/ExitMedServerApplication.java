3d16ed6c933a7548ce37e35cdc2844c36e310ab2
DrRivaski <48974215+DrRivaski@users.noreply.github.com>
2023-08-14 17:00:48 +0900

    initial commit

---
+++ src/main/java/com/example/exitmedserver/ExitMedServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.exitmedserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExitMedServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExitMedServerApplication.class, args);
+    }
+
+}
