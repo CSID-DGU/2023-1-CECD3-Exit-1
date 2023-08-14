2825aa56e8626b6aa94a8515317e43cd800d2461
82106 <eldms0406@naver.com>
2023-08-14 14:42:25 +0900

    [feat] #9 홈 뷰 -> 검색뷰 화면 이동 연결

--- app/src/main/java/com/cecd/exitmed/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/cecd/exitmed/presentation/home/HomeActivity.kt
@@ -1,8 +1,10 @@
 package com.cecd.exitmed.presentation.home
 
+import android.content.Intent
 import android.os.Bundle
 import com.cecd.exitmed.R
 import com.cecd.exitmed.databinding.ActivityHomeBinding
+import com.cecd.exitmed.presentation.search.SearchActivity
 import com.cecd.exitmed.util.binding.BindingActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,5 +12,17 @@ import dagger.hilt.android.AndroidEntryPoint
 class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        addListeners()
+    }
+
+    private fun addListeners() {
+        binding.layoutHomeSearchBox.setOnClickListener {
+            moveToSearch()
+        }
+    }
+
+    private fun moveToSearch() {
+        startActivity(Intent(this, SearchActivity::class.java))
     }
 }
