a6f62dc90bb039a5e09ed4a0feac3116bbccfff8
DrRivaski <48974215+DrRivaski@users.noreply.github.com>
2023-08-16 17:00:17 +0900

    [add] #13 User Entity 생성

---
+++ src/main/java/com/example/exitmedserver/user/User.java
@@ -0,0 +1,22 @@
+package com.example.exitmedserver.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+public class User {
+    @Id
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "user_password")
+    private String userPassword;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+}
