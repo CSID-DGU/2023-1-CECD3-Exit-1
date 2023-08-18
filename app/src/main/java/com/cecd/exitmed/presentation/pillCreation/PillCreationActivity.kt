e24637cfcb1b4e1464e62781fac057a7fee36d15
82106 <eldms0406@naver.com>
2023-08-18 22:30:34 +0900

    [feat] #11 약 서랍 생성 구현

---
+++ app/src/main/java/com/cecd/exitmed/presentation/pillCreation/PillCreationActivity.kt
@@ -0,0 +1,83 @@
+package com.cecd.exitmed.presentation.pillCreation
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.viewpager2.widget.ViewPager2
+import com.cecd.exitmed.R
+import com.cecd.exitmed.databinding.ActivityPillCreationBinding
+import com.cecd.exitmed.util.binding.BindingActivity
+import com.google.android.material.tabs.TabLayoutMediator
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+@AndroidEntryPoint
+class PillCreationActivity :
+    BindingActivity<ActivityPillCreationBinding>(R.layout.activity_pill_creation) {
+    private val viewModel: PillCreationViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        addListeners()
+        collectData()
+        setPillCreationPagerAdapter()
+    }
+
+    private fun addListeners() {
+        binding.btnNext.setOnClickListener {
+            binding.vpPillCreation.currentItem++
+        }
+        binding.btnBack.setOnClickListener {
+            when (binding.vpPillCreation.currentItem) {
+                0 -> {}
+                else -> {
+                    binding.vpPillCreation.currentItem--
+                }
+            }
+        }
+        binding.ivClose.setOnClickListener {
+            // TODO 화면 finish()
+        }
+    }
+
+    private fun collectData() {
+        viewModel.pagePosition.flowWithLifecycle(lifecycle).onEach { position ->
+            binding.btnNext.setText(if (position == 3) R.string.pill_creation else R.string.next)
+            binding.tvPillCreationStep.text =
+                String.format(getString(R.string.pill_creation_step), position + 1)
+        }.launchIn(lifecycleScope)
+    }
+
+    private fun getPageChangeCallback() =
+        object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+                viewModel.setPagePosition(position)
+            }
+        }
+
+    private fun setPillCreationPagerAdapter() {
+        binding.vpPillCreation.isUserInputEnabled = false
+
+        val fragmentList = ArrayList<Fragment>()
+        fragmentList.add(PillCreationLastDateFragment())
+        fragmentList.add(PillCreationPeriodFragment())
+        fragmentList.add(PillCreationTimeFragment())
+        fragmentList.add(PillCreationMemoFragment())
+
+        val adapter = PillCreationPagerAdapter(fragmentList, this)
+        with(binding.vpPillCreation) {
+            this.adapter = adapter
+            this.registerOnPageChangeCallback(getPageChangeCallback())
+        }
+
+        TabLayoutMediator(
+            binding.pillCreationTabIndicator, binding.vpPillCreation
+        ) { _, _ -> }.attach()
+
+    }
+}
