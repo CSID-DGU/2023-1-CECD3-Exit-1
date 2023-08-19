497d03c17c5d35805c4abb7b94be494a45766eca
82106 <eldms0406@naver.com>
2023-08-19 14:11:35 +0900

    [feat] #15 마이페이지에서 약서랍 리스트로 화면 이동 구현

--- app/src/main/java/com/cecd/exitmed/presentation/my/MyActivity.kt
+++ app/src/main/java/com/cecd/exitmed/presentation/my/MyActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import com.cecd.exitmed.R
 import com.cecd.exitmed.databinding.ActivityMyBinding
+import com.cecd.exitmed.presentation.pillDrawer.PillDrawerListActivity
 import com.cecd.exitmed.util.binding.BindingActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,6 +19,9 @@ class MyActivity : BindingActivity<ActivityMyBinding>(R.layout.activity_my) {
         binding.layoutMyPageBookmark.setOnClickListener {
             moveToPillBookMark()
         }
+        binding.layoutMyPagePillDrawer.setOnClickListener {
+            moveToPillDrawer()
+        }
         binding.layoutToolbar.ivBack.setOnClickListener {
             finish()
         }
@@ -25,5 +29,9 @@ class MyActivity : BindingActivity<ActivityMyBinding>(R.layout.activity_my) {
 
     private fun moveToPillBookMark() {
         startActivity(Intent(this, MyPillListActivity::class.java))
+    }
+
+    private fun moveToPillDrawer() {
+        startActivity(Intent(this, PillDrawerListActivity::class.java))
     }
 }
