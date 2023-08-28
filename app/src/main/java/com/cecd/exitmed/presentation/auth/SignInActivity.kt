be271a141dbc7d86ee4ac6099ee29f0be866e06e
82106 <eldms0406@naver.com>
2023-08-28 20:59:59 +0900

    [feat] #26 로그인 뷰에서 홈 뷰로 이동

--- app/src/main/java/com/cecd/exitmed/presentation/auth/SignInActivity.kt
+++ app/src/main/java/com/cecd/exitmed/presentation/auth/SignInActivity.kt
@@ -2,17 +2,27 @@ package com.cecd.exitmed.presentation.auth
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.cecd.exitmed.R
 import com.cecd.exitmed.databinding.ActivitySignInBinding
+import com.cecd.exitmed.presentation.home.HomeActivity
 import com.cecd.exitmed.util.binding.BindingActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class SignInActivity : BindingActivity<ActivitySignInBinding>(R.layout.activity_sign_in) {
+    private val authViewModel: SignViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.viewModel = authViewModel
+        binding.lifecycleOwner = this
 
         addListeners()
+        collectData()
     }
 
     private fun addListeners() {
@@ -21,7 +31,23 @@ class SignInActivity : BindingActivity<ActivitySignInBinding>(R.layout.activity_
         }
     }
 
+    private fun collectData() {
+        authViewModel.isCompleteSignIn.flowWithLifecycle(lifecycle).onEach { isCompleteSignIn ->
+            when (isCompleteSignIn) {
+                true -> {
+                    moveToHome()
+                }
+
+                else -> {}
+            }
+        }.launchIn(lifecycleScope)
+    }
+
     private fun moveToSignUp() {
         startActivity(Intent(this, SignUpEmailActivity::class.java))
+    }
+
+    private fun moveToHome() {
+        startActivity(Intent(this, HomeActivity::class.java))
     }
 }
