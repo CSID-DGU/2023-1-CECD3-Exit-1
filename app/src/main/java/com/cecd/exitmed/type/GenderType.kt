a6d00254ea698d5b880bcd6733f09eafb4615f07
82106 <eldms0406@naver.com>
2023-08-28 20:29:51 +0900

    [chore] #24 ktlint 적용

--- app/src/main/java/com/cecd/exitmed/type/GenderType.kt
+++ app/src/main/java/com/cecd/exitmed/type/GenderType.kt
@@ -3,9 +3,7 @@ package com.cecd.exitmed.type
 import androidx.annotation.StringRes
 import com.cecd.exitmed.R
 
-enum class GenderType(
-    @StringRes val gender: Int, val genderRequest: String
-) {
+enum class GenderType(@StringRes val gender: Int, val genderRequest: String) {
     MALE(R.string.sign_up_info_gender_male, "male"), FEMALE(
         R.string.sign_up_info_gender_female, "female"
     ),
