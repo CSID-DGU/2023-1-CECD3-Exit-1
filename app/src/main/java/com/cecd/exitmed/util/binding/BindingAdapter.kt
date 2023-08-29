023d6963fbdec110c782f9eabcc76771717ae11f
82106 <eldms0406@naver.com>
2023-08-29 15:19:06 +0900

    [feat] #22 []로 감싸진 부분에만 다른 색이 될 수 있도록 하는 BindingAdapter구현

--- app/src/main/java/com/cecd/exitmed/util/binding/BindingAdapter.kt
+++ app/src/main/java/com/cecd/exitmed/util/binding/BindingAdapter.kt
@@ -1,7 +1,12 @@
 package com.cecd.exitmed.util.binding
 
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.View
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.cecd.exitmed.R
 
 @BindingAdapter("visibility")
 fun View.setVisibility(isVisible: Boolean?) {
@@ -13,3 +18,33 @@ fun View.setVisibility(isVisible: Boolean?) {
 fun View.setSelected(isSelected: Boolean) {
     this.isSelected = isSelected
 }
+
+@BindingAdapter("coloredText")
+fun TextView.setColoredText(text: String?) {
+    text?.let {
+        val pattern = "\\[(.*?)]".toRegex()
+        val matches = pattern.findAll(text)
+
+        val spannableStringBuilder = SpannableStringBuilder(text)
+
+        for (match in matches) {
+            val start = match.range.first
+            val end = match.range.last + 1
+
+            val color = R.color.orange_700
+            val span = ForegroundColorSpan(context.getColor(color))
+            spannableStringBuilder.setSpan(
+                span,
+                start + 1,
+                end - 1,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+
+            spannableStringBuilder.replace(start, start + 1, "")
+            spannableStringBuilder.replace(end - 2, end - 1, "")
+        }
+        this.text = spannableStringBuilder
+    }
+}
+
+
