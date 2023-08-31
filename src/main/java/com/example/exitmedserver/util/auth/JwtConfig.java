e8f288af9897fd7523a048dae9ca948d2d8dd489
DrRivaski <48974215+DrRivaski@users.noreply.github.com>
2023-08-31 14:57:24 +0900

    [mod] #18 JWT Expiration time 4주로 수정

--- src/main/java/com/example/exitmedserver/util/auth/JwtConfig.java
+++ src/main/java/com/example/exitmedserver/util/auth/JwtConfig.java
@@ -3,6 +3,6 @@ package com.example.exitmedserver.util.auth;
 public class JwtConfig {
     public String subject = "exitToken";
     public String secret = "exit";
-    public int accessExpirationTime = 60 * 60 * 1000;
+    public Long accessExpirationTime = 60 * 60 * 24 * 7 * 4 * 1000L;
     public int refreshExpirationTime = 60 * 60 * 24 * 7 * 1000;
 }
