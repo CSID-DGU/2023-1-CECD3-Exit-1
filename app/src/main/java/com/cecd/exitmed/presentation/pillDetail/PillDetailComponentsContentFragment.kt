bacd4f0bed49e0a0d1b4081371c5d175eb229b5b
82106 <eldms0406@naver.com>
2023-09-04 20:44:06 +0900

    [feat] #29 약 상세뷰 - 성분/함량 페이지 디자인 및 구현

---
+++ app/src/main/java/com/cecd/exitmed/presentation/pillDetail/PillDetailComponentsContentFragment.kt
@@ -0,0 +1,17 @@
+package com.cecd.exitmed.presentation.pillDetail
+
+import android.os.Bundle
+import android.view.View
+import com.cecd.exitmed.R
+import com.cecd.exitmed.databinding.FragmentPillDetailComponentsContentBinding
+import com.cecd.exitmed.databinding.FragmentPillDetailMedicationBinding
+import com.cecd.exitmed.util.binding.BindingFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PillDetailComponentsContentFragment :
+    BindingFragment<FragmentPillDetailComponentsContentBinding>(R.layout.fragment_pill_detail_components_content) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
