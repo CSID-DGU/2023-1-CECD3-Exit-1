45fce7fd7fd41e1bccb98c84a5acd08629ebaff8
82106 <eldms0406@naver.com>
2023-09-04 20:44:42 +0900

    [feat] #29 약 상세뷰 - DUR 페이지 디자인 및 구현

---
+++ app/src/main/java/com/cecd/exitmed/presentation/pillDetail/PillDetailDURFragment.kt
@@ -0,0 +1,17 @@
+package com.cecd.exitmed.presentation.pillDetail
+
+import android.os.Bundle
+import android.view.View
+import com.cecd.exitmed.R
+import com.cecd.exitmed.databinding.FragmentPillDetailDurBinding
+import com.cecd.exitmed.databinding.FragmentPillDetailMedicationBinding
+import com.cecd.exitmed.util.binding.BindingFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PillDetailDURFragment :
+    BindingFragment<FragmentPillDetailDurBinding>(R.layout.fragment_pill_detail_dur) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
