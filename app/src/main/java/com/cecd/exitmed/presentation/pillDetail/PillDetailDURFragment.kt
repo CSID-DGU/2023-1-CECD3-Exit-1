d94e61d0cf13d7d026cfbc2489a5db93a9366856
82106 <eldms0406@naver.com>
2023-09-11 20:48:11 +0900

    [feat] #33 뷰모델 내 flow 자료형 UiState로 수정 및 UiState에 따른 분기 처리

--- app/src/main/java/com/cecd/exitmed/presentation/pillDetail/PillDetailDURFragment.kt
+++ app/src/main/java/com/cecd/exitmed/presentation/pillDetail/PillDetailDURFragment.kt
@@ -2,11 +2,17 @@ package com.cecd.exitmed.presentation.pillDetail
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.cecd.exitmed.R
 import com.cecd.exitmed.databinding.FragmentPillDetailDurBinding
+import com.cecd.exitmed.util.UiState
 import com.cecd.exitmed.util.binding.BindingFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class PillDetailDURFragment :
@@ -18,6 +24,50 @@ class PillDetailDURFragment :
         binding.lifecycleOwner = this.viewLifecycleOwner
 
         addListeners()
+        collectData()
+    }
+
+    private fun collectData() {
+        durViewModel.durAgeProhibition.flowWithLifecycle(lifecycle).onEach {
+            setDURContents(
+                uiState = it,
+                contentLayout = binding.layoutPillDetailDurAgeProhibit,
+                contentTextView = binding.tvPillDetailDurAgeProhibitContent,
+                resInitContentString = R.string.init_dur_age_prohibition_contents
+            )
+        }.launchIn(lifecycleScope)
+        durViewModel.durPregnantProhibition.flowWithLifecycle(lifecycle).onEach {
+            setDURContents(
+                uiState = it,
+                contentLayout = binding.layoutPillDetailDurPregnantProhibit,
+                contentTextView = binding.tvPillDetailDurPregnantProhibitContent,
+                resInitContentString = R.string.init_dur_pregnant_prohibition_contents
+            )
+        }.launchIn(lifecycleScope)
+        durViewModel.durCapacityCaution.flowWithLifecycle(lifecycle).onEach {
+            setDURContents(
+                uiState = it,
+                contentLayout = binding.layoutPillDetailDurCapacityCaution,
+                contentTextView = binding.tvPillDetailDurCapacityCautionContent,
+                resInitContentString = R.string.init_dur_capacity_caution_contents
+            )
+        }.launchIn(lifecycleScope)
+        durViewModel.durAdministrationDurationCaution.flowWithLifecycle(lifecycle).onEach {
+            setDURContents(
+                uiState = it,
+                contentLayout = binding.layoutPillDetailDurAdministrationDurationCaution,
+                contentTextView = binding.tvPillDetailDurAdministrationDurationCautionContent,
+                resInitContentString = R.string.init_dur_administration_duration_caution_contents
+            )
+        }.launchIn(lifecycleScope)
+        durViewModel.durSeniorCaution.flowWithLifecycle(lifecycle).onEach {
+            setDURContents(
+                uiState = it,
+                contentLayout = binding.layoutPillDetailDurElderCaution,
+                contentTextView = binding.tvPillDetailDurElderCautionContent,
+                resInitContentString = R.string.init_dur_senior_caution_contents
+            )
+        }.launchIn(lifecycleScope)
     }
 
     private fun addListeners() {
@@ -26,6 +76,35 @@ class PillDetailDURFragment :
         }
         binding.ivDurMeaningClose.setOnClickListener {
             binding.layoutDurMeaningBubble.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun setDURContents(
+        uiState: UiState<String?>,
+        contentLayout: View,
+        contentTextView: TextView,
+        resInitContentString: Int
+    ) {
+        when (uiState) {
+            is UiState.Loading -> {
+                contentTextView.text =
+                    getString(R.string.loading_dur_contents)
+            }
+
+            is UiState.Init -> {
+                contentTextView.text =
+                    getString(resInitContentString)
+            }
+
+            is UiState.Success -> {
+                contentTextView.text = uiState.data
+            }
+
+            is UiState.Error -> {
+                contentLayout.visibility = View.GONE
+            }
+
+            else -> {}
         }
     }
 }
--- app/src/main/java/com/cecd/exitmed/presentation/pillDetail/PillDetailDURViewModel.kt
+++ app/src/main/java/com/cecd/exitmed/presentation/pillDetail/PillDetailDURViewModel.kt
@@ -3,6 +3,7 @@ package com.cecd.exitmed.presentation.pillDetail
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.cecd.exitmed.domain.repository.DURRepository
+import com.cecd.exitmed.util.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -14,36 +15,37 @@ import javax.inject.Inject
 class PillDetailDURViewModel @Inject constructor(
     private val durRepository: DURRepository
 ) : ViewModel() {
-    private val _durAgeProhibition = MutableStateFlow<String?>(null)
+    private val _durAgeProhibition = MutableStateFlow<UiState<String?>>(UiState.Loading)
     val durAgeProhibition get() = _durAgeProhibition.asStateFlow()
-    private val _durPregnantProhibition = MutableStateFlow<String?>(null)
+    private val _durPregnantProhibition = MutableStateFlow<UiState<String?>>(UiState.Loading)
     val durPregnantProhibition get() = _durPregnantProhibition.asStateFlow()
-    private val _durCapacityCaution = MutableStateFlow<String?>(null)
+    private val _durCapacityCaution = MutableStateFlow<UiState<String?>>(UiState.Loading)
     val durCapacityCaution get() = _durCapacityCaution.asStateFlow()
-    private val _durAdministrationDurationCaution = MutableStateFlow<String?>(null)
+    private val _durAdministrationDurationCaution =
+        MutableStateFlow<UiState<String?>>(UiState.Loading)
     val durAdministrationDurationCaution get() = _durAdministrationDurationCaution.asStateFlow()
-    private val _durSeniorCaution = MutableStateFlow<String?>(null)
+    private val _durSeniorCaution = MutableStateFlow<UiState<String?>>(UiState.Loading)
     val durSeniorCaution get() = _durSeniorCaution.asStateFlow()
 
     init {
-        fetchDURAgeProhibitionContents("201309959")
-        fetchDURPregnantProhibitionContents("201309959")
-        fetchDURCapacityCautionContents("201309959")
-        fetchDURAdministrationDurationCaution("201309959")
-        fetchDURSeniorCautionContents("201309959")
+        fetchDURAgeProhibitionContents(ITEM_SEQ)
+        fetchDURPregnantProhibitionContents(ITEM_SEQ)
+        fetchDURCapacityCautionContents(ITEM_SEQ)
+        fetchDURAdministrationDurationCaution(ITEM_SEQ)
+        fetchDURSeniorCautionContents(ITEM_SEQ)
     }
 
     private fun fetchDURAgeProhibitionContents(itemSeq: String) {
         viewModelScope.launch {
             durRepository.fetchDURAgeProhibition(itemSeq)
                 .onSuccess { item ->
-                    _durAgeProhibition.value =
-                        if (item[0].REMARK != null)
-                            item[0].PROHBT_CONTENT + item[0].REMARK
-                        else
-                            item[0].PROHBT_CONTENT
+                    if (item[0].PROHBT_CONTENT == null)
+                        _durAgeProhibition.value = UiState.Init
+                    else
+                        _durAgeProhibition.value = UiState.Success(item[0].PROHBT_CONTENT)
                 }
                 .onFailure { throwable ->
+                    _durAgeProhibition.value = UiState.Error(throwable.message)
                     Timber.d(throwable.message)
                 }
         }
@@ -53,13 +55,13 @@ class PillDetailDURViewModel @Inject constructor(
         viewModelScope.launch {
             durRepository.fetchDURPregnantProhibition(itemSeq)
                 .onSuccess { item ->
-                    _durPregnantProhibition.value =
-                        if (item[0].REMARK != null)
-                            item[0].PROHBT_CONTENT + item[0].REMARK
-                        else
-                            item[0].PROHBT_CONTENT
+                    if (item[0].PROHBT_CONTENT == null)
+                        _durPregnantProhibition.value = UiState.Init
+                    else
+                        _durPregnantProhibition.value = UiState.Success(item[0].PROHBT_CONTENT)
                 }
                 .onFailure { throwable ->
+                    _durPregnantProhibition.value = UiState.Error(throwable.message)
                     Timber.d(throwable.message)
                 }
         }
@@ -69,13 +71,13 @@ class PillDetailDURViewModel @Inject constructor(
         viewModelScope.launch {
             durRepository.fetchDURCapacityCaution(itemSeq)
                 .onSuccess { item ->
-                    _durCapacityCaution.value =
-                        if (item[0].REMARK != null)
-                            item[0].PROHBT_CONTENT + item[0].REMARK
-                        else
-                            item[0].PROHBT_CONTENT
+                    if (item[0].PROHBT_CONTENT == null)
+                        _durCapacityCaution.value = UiState.Init
+                    else
+                        _durCapacityCaution.value = UiState.Success(item[0].PROHBT_CONTENT)
                 }
                 .onFailure { throwable ->
+                    _durCapacityCaution.value = UiState.Error(throwable.message)
                     Timber.d(throwable.message)
                 }
         }
@@ -85,13 +87,14 @@ class PillDetailDURViewModel @Inject constructor(
         viewModelScope.launch {
             durRepository.fetchDURAdministrationDurationCaution(itemSeq)
                 .onSuccess { item ->
-                    _durAdministrationDurationCaution.value =
-                        if (item[0].REMARK != null)
-                            item[0].PROHBT_CONTENT + item[0].REMARK
-                        else
-                            item[0].PROHBT_CONTENT
+                    if (item[0].PROHBT_CONTENT == null)
+                        _durAdministrationDurationCaution.value = UiState.Init
+                    else
+                        _durAdministrationDurationCaution.value =
+                            UiState.Success(item[0].PROHBT_CONTENT)
                 }
                 .onFailure { throwable ->
+                    _durAdministrationDurationCaution.value = UiState.Error(throwable.message)
                     Timber.d(throwable.message)
                 }
         }
@@ -101,15 +104,19 @@ class PillDetailDURViewModel @Inject constructor(
         viewModelScope.launch {
             durRepository.fetchDURSeniorCaution(itemSeq)
                 .onSuccess { item ->
-                    _durSeniorCaution.value =
-                        if (item[0].REMARK != null)
-                            item[0].PROHBT_CONTENT + item[0].REMARK
-                        else
-                            item[0].PROHBT_CONTENT
+                    if (item[0].PROHBT_CONTENT == null)
+                        _durSeniorCaution.value = UiState.Init
+                    else
+                        _durSeniorCaution.value = UiState.Success(item[0].PROHBT_CONTENT)
                 }
                 .onFailure { throwable ->
+                    _durSeniorCaution.value = UiState.Error(throwable.message)
                     Timber.d(throwable.message)
                 }
         }
+    }
+
+    companion object {
+        const val ITEM_SEQ = "197200484"
     }
 }
