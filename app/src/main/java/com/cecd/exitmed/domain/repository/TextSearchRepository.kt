98b83c26e9ad67f12730012acb47432265af5c3d
82106 <eldms0406@naver.com>
2023-09-29 20:27:08 +0900

    [chore] #42 ktlint 적용

--- app/src/main/java/com/cecd/exitmed/domain/repository/TextSearchRepository.kt
+++ app/src/main/java/com/cecd/exitmed/domain/repository/TextSearchRepository.kt
@@ -1,6 +1,5 @@
 package com.cecd.exitmed.domain.repository
 
-import com.cecd.exitmed.data.model.response.ResponseRecentSearchTerm
 import com.cecd.exitmed.domain.type.SearchPill
 
 interface TextSearchRepository {
