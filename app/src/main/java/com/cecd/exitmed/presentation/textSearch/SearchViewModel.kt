ba81c572a81f602fbfea3f45bab8c76e52004643
82106 <eldms0406@naver.com>
2023-09-29 19:57:39 +0900

    [feat] #42 텍스트 검색 뷰모델 구현, 텍스트 검색 결과 리스트, 검색 결과 개수 데이터 조작

--- app/src/main/java/com/cecd/exitmed/presentation/textSearch/SearchViewModel.kt
+++ app/src/main/java/com/cecd/exitmed/presentation/textSearch/SearchViewModel.kt
@@ -1,18 +1,39 @@
 package com.cecd.exitmed.presentation.textSearch
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.cecd.exitmed.domain.repository.TextSearchRepository
 import com.cecd.exitmed.domain.type.SearchPill
 import com.cecd.exitmed.util.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
-class SearchViewModel @Inject constructor() : ViewModel() {
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+    private val textSearchRepository: TextSearchRepository
+) : ViewModel() {
     val searchText = MutableStateFlow("")
     private var _searchCount = MutableStateFlow<Int?>(5)
     val searchCount get() = _searchCount.asStateFlow()
     private var _searchList = MutableStateFlow<UiState<List<SearchPill>>>(UiState.Empty)
     val searchList get() = _searchList.asStateFlow()
+
+    fun textPillSearch() {
+        viewModelScope.launch {
+            textSearchRepository.textPillSearch(searchText.value)
+                .onSuccess { searchList ->
+                    _searchList.value = UiState.Success(searchList)
+                    _searchCount.value = UiState.Success(searchList).data.size
+                }
+                .onFailure { throwable ->
+                    Timber.e(throwable.message)
+                }
+        }
+    }
 
     val mockHistoryList = listOf(
         "타이레놀",
@@ -28,33 +49,5 @@ class SearchViewModel @Inject constructor() : ViewModel() {
         "인데놀정",
         "환인트라조돈캡슐",
         "타이레놀"
-    )
-
-    val mockSearchList = listOf(
-        SearchPill(
-            1,
-            "타이레놀",
-            "진통제"
-        ),
-        SearchPill(
-            1,
-            "타이레놀",
-            "진통제"
-        ),
-        SearchPill(
-            1,
-            "타이레놀",
-            "진통제"
-        ),
-        SearchPill(
-            1,
-            "타이레놀",
-            "진통제"
-        ),
-        SearchPill(
-            1,
-            "타이레놀",
-            "진통제"
-        ),
     )
 }
