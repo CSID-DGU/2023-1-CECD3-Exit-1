f0b47a61405a19cf3c3eaf14746bd61be1051740
DrRivaski <48974215+DrRivaski@users.noreply.github.com>
2023-09-30 22:40:55 +0900

    [feat] #51 복용일을 제외한 약서랍 상세조회 기능 구현

--- src/main/java/com/example/exitmedserver/pill/controller/PillController.java
+++ src/main/java/com/example/exitmedserver/pill/controller/PillController.java
@@ -1,8 +1,6 @@
 package com.example.exitmedserver.pill.controller;
 
-import com.example.exitmedserver.pill.dto.PillAddDrawerRequestDto;
-import com.example.exitmedserver.pill.dto.PillAddDrawerResponseDto;
-import com.example.exitmedserver.pill.dto.PillGetDrawerListResponse;
+import com.example.exitmedserver.pill.dto.*;
 import com.example.exitmedserver.pill.service.PillService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class PillController {
     private final PillService pillService;
+
     @PostMapping("/auth/pill/add-to-drawer")
     public PillAddDrawerResponseDto addDrawer(@RequestHeader("Authorization") String jwtToken, @RequestBody PillAddDrawerRequestDto pillAddDrawerRequestDto) {
         return pillService.addDrawer(jwtToken, pillAddDrawerRequestDto);
@@ -19,5 +18,10 @@ public class PillController {
     @GetMapping("/auth/pill/drawer")
     public PillGetDrawerListResponse getDrawerList(@RequestHeader("Authorization") String jwtToken) {
         return pillService.getDrawerList(jwtToken);
+    }
+
+    @GetMapping("/auth/pill/drawer/{pillItemSequence}")
+    public PillGetPillInDrawerResponseDto getPillInDrawer(@RequestHeader("Authorization") String jwtToken, @PathVariable Long pillItemSequence) {
+        return pillService.getPillInDrawer(jwtToken, pillItemSequence);
     }
 }
--- src/main/java/com/example/exitmedserver/pill/dto/PillGetPillInDrawerResponseDto.java
+++ src/main/java/com/example/exitmedserver/pill/dto/PillGetPillInDrawerResponseDto.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class PillGetPillInDrawerResponseDto {
     private String pillName;
+    private String imageLink;
     private String alarm;
     private String finalDate;
     private String dosage;
