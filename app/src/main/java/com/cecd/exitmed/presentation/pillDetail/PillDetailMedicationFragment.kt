3f6fc6fec78e2bdd6aafc0745c5e18e77e416f26
82106 <eldms0406@naver.com>
2023-10-01 17:04:58 +0900

    [feat] #58 의약품 상세 - 복용뷰 구현

--- app/src/main/java/com/cecd/exitmed/presentation/pillDetail/PillDetailMedicationFragment.kt
+++ app/src/main/java/com/cecd/exitmed/presentation/pillDetail/PillDetailMedicationFragment.kt
@@ -2,15 +2,32 @@ package com.cecd.exitmed.presentation.pillDetail
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.cecd.exitmed.R
 import com.cecd.exitmed.databinding.FragmentPillDetailMedicationBinding
 import com.cecd.exitmed.util.binding.BindingFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class PillDetailMedicationFragment :
     BindingFragment<FragmentPillDetailMedicationBinding>(R.layout.fragment_pill_detail_medication) {
+    private val pillDetailViewModel: PillDetailViewModel by activityViewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        collectData()
+    }
+
+    private fun collectData() {
+        pillDetailViewModel.pillEffect.flowWithLifecycle(lifecycle).onEach { pillEffect->
+            binding.tvPillDetailMedicationEffectContent.text = pillEffect
+        }.launchIn(lifecycleScope)
+        pillDetailViewModel.pillStorageMethod.flowWithLifecycle(lifecycle).onEach { pillStorageMethod->
+            binding.tvPillDetailMedicationStorageMethodContent.text = pillStorageMethod
+        }.launchIn(lifecycleScope)
     }
 }
