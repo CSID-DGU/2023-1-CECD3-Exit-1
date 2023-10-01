dd698e7af5883dbd50f0770a0351b0506083c3df
DrRivaski <leesm990708@gmail.com>
2023-10-01 17:33:25 +0900

    [add] #48 약서랍 추가하기에서 요일 객체 추가 - DayForDrawer

---
+++ src/main/java/com/example/exitmedserver/pill/dto/DayForDrawer.java
@@ -0,0 +1,14 @@
+package com.example.exitmedserver.pill.dto;
+
+import lombok.Data;
+
+@Data
+public class DayForDrawer {
+    private boolean monday;
+    private boolean tuesday;
+    private boolean wednesday;
+    private boolean thursday;
+    private boolean friday;
+    private boolean saturday;
+    private boolean sunday;
+}
