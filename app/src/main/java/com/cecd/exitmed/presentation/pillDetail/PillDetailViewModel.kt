382c47ea2ddbf572e697c36ace9d8b9e36a867ef
82106 <eldms0406@naver.com>
2023-10-01 17:03:25 +0900

    [feat] #58 의약품 상세뷰 viewModel 구현

---
+++ app/src/main/java/com/cecd/exitmed/presentation/pillDetail/PillDetailViewModel.kt
@@ -0,0 +1,55 @@
+package com.cecd.exitmed.presentation.pillDetail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.cecd.exitmed.domain.repository.PillDetailRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class PillDetailViewModel @Inject constructor(
+    private val pillDetailRepository: PillDetailRepository
+) : ViewModel() {
+    private val _pillName = MutableStateFlow("")
+    val pillName get() = _pillName.asStateFlow()
+    private val _pillClassification = MutableStateFlow("")
+    val pillClassification get() = _pillClassification.asStateFlow()
+    private val _pillImage = MutableStateFlow("")
+    val pillImage get() = _pillImage.asStateFlow()
+    private val _isBookMarked = MutableStateFlow(false)
+    val isBookMarked get() = _isBookMarked.asStateFlow()
+    private val _pillEffect = MutableStateFlow("")
+    val pillEffect get() = _pillEffect.asStateFlow()
+    private val _pillStorageMethod = MutableStateFlow("")
+    val pillStorageMethod get() = _pillStorageMethod.asStateFlow()
+    private val _pillIngredient = MutableStateFlow("")
+    val pillIngredient get() = _pillIngredient.asStateFlow()
+    private val _pillDosageMethod = MutableStateFlow("")
+    val pillDosageMethod get() = _pillDosageMethod.asStateFlow()
+    private val _pillCaution = MutableStateFlow("")
+    val pillCaution get() = _pillCaution.asStateFlow()
+
+    fun fetchPillDetail(itemSeq: Int) {
+        viewModelScope.launch {
+            pillDetailRepository.fetchPillDetail(itemSeq)
+                .onSuccess { pillDetail ->
+                    _pillName.value = pillDetail.pillName
+                    _pillClassification.value = pillDetail.classification
+                    _pillImage.value = pillDetail.imageLink
+                    _isBookMarked.value = pillDetail.favorite
+                    _pillEffect.value = pillDetail.effect
+                    _pillStorageMethod.value = pillDetail.storage
+                    _pillIngredient.value = pillDetail.ingredient
+                    _pillDosageMethod.value = pillDetail.dosage
+                    _pillCaution.value = pillDetail.warning
+                }
+                .onFailure { throwable ->
+                    Timber.e(throwable.message)
+                }
+        }
+    }
+}
