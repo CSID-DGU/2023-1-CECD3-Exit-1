66d204d105c2eaecacf2d57539dca37828ecf3e8
82106 <eldms0406@naver.com>
2023-10-01 17:05:14 +0900

    [feat] #58 의약품 상세 - 용법뷰 구현

--- app/src/main/java/com/cecd/exitmed/presentation/pillDetail/PillDetailUsageFragment.kt
+++ app/src/main/java/com/cecd/exitmed/presentation/pillDetail/PillDetailUsageFragment.kt
@@ -2,15 +2,29 @@ package com.cecd.exitmed.presentation.pillDetail
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.cecd.exitmed.R
 import com.cecd.exitmed.databinding.FragmentPillDetailUsageBinding
 import com.cecd.exitmed.util.binding.BindingFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class PillDetailUsageFragment :
     BindingFragment<FragmentPillDetailUsageBinding>(R.layout.fragment_pill_detail_usage) {
+    private val pillDetailViewModel: PillDetailViewModel by activityViewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        collectData()
+    }
+
+    private fun collectData() {
+        pillDetailViewModel.pillDosageMethod.flowWithLifecycle(lifecycle)
+            .onEach { pillDosageMethod ->
+                binding.tvPillDetailUsageContent.text = pillDosageMethod
+            }.launchIn(lifecycleScope)
     }
 }
