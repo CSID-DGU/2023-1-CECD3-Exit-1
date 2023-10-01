94c458516b5968aacdf11fd2a55edeced425689d
82106 <eldms0406@naver.com>
2023-10-01 17:05:21 +0900

    [feat] #58 의약품 상세 - 주의뷰 구현

--- app/src/main/java/com/cecd/exitmed/presentation/pillDetail/PillDetailCautionFragment.kt
+++ app/src/main/java/com/cecd/exitmed/presentation/pillDetail/PillDetailCautionFragment.kt
@@ -2,15 +2,28 @@ package com.cecd.exitmed.presentation.pillDetail
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.cecd.exitmed.R
-import com.cecd.exitmed.databinding.FragmentPillDetailUsageBinding
+import com.cecd.exitmed.databinding.FragmentPillDetailCautionBinding
 import com.cecd.exitmed.util.binding.BindingFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class PillDetailCautionFragment :
-    BindingFragment<FragmentPillDetailUsageBinding>(R.layout.fragment_pill_detail_caution) {
+    BindingFragment<FragmentPillDetailCautionBinding>(R.layout.fragment_pill_detail_caution) {
+    private val pillDetailViewModel: PillDetailViewModel by activityViewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        collectData()
+    }
+
+    private fun collectData() {
+        pillDetailViewModel.pillCaution.flowWithLifecycle(lifecycle).onEach { pillCaution ->
+            binding.tvPillDetailCautionContent.text = pillCaution
+        }.launchIn(lifecycleScope)
     }
 }
