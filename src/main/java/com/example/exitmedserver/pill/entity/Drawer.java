257b764c7fbb48c020333dda17cc03800eb2c20a
DrRivaski <leesm990708@gmail.com>
2023-10-01 17:48:43 +0900

    [add] #48 Drawer Entity 수정 - 요일 column 추가

--- src/main/java/com/example/exitmedserver/pill/entity/Drawer.java
+++ src/main/java/com/example/exitmedserver/pill/entity/Drawer.java
@@ -32,13 +32,42 @@ public class Drawer {
     @Column(name = "comment")
     private String comment;
 
+    @Column(name = "monday", columnDefinition = "TINYINT")
+    private boolean monday;
+
+    @Column(name = "tuesday", columnDefinition = "TINYINT")
+    private boolean tuesday;
+
+    @Column(name = "wednesday", columnDefinition = "TINYINT")
+    private boolean wednesday;
+
+    @Column(name = "thursday", columnDefinition = "TINYINT")
+    private boolean thursday;
+
+    @Column(name = "friday", columnDefinition = "TINYINT")
+    private boolean friday;
+
+    @Column(name = "saturday", columnDefinition = "TINYINT")
+    private boolean saturday;
+
+    @Column(name = "sunday", columnDefinition = "TINYINT")
+    private boolean sunday;
+
     @Builder
-    public Drawer(Integer id, String userId, Long pillItemSequence, Date finalDate, String dosageCycle, String comment) {
+    public Drawer(Integer id, String userId, Long pillItemSequence, Date finalDate, String dosageCycle, String comment,
+                  boolean monday, boolean tuesday, boolean wednesday, boolean thursday, boolean friday, boolean saturday, boolean sunday) {
         this.id = id;
         this.userId = userId;
         this.pillItemSequence = pillItemSequence;
         this.finalDate = finalDate;
         this.dosageCycle = dosageCycle;
         this.comment = comment;
+        this.monday = monday;
+        this.tuesday = tuesday;
+        this.wednesday = wednesday;
+        this.thursday = thursday;
+        this.friday = friday;
+        this.saturday = saturday;
+        this.sunday = sunday;
     }
 }
