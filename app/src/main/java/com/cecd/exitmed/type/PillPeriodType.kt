0735e8375fa5aeb5e35fd3e350295917ea28b4f7
Dani43 <eldms0406@naver.com>
2023-11-17 15:50:49 +0900

    [chore] #71 복용 주기(매일, 고정된 날) 상수명 수정

--- app/src/main/java/com/cecd/exitmed/type/PillPeriodType.kt
+++ app/src/main/java/com/cecd/exitmed/type/PillPeriodType.kt
@@ -4,5 +4,5 @@ import androidx.annotation.StringRes
 import com.cecd.exitmed.R
 
 enum class PillPeriodType(@StringRes val titleRes: Int) {
-    EVERYDAY(R.string.pill_creation_everyday), FIXEDDAY(R.string.pill_creation_fixed_day)
+    EVERY_DAY(R.string.pill_creation_everyday), FIXED_DAY(R.string.pill_creation_fixed_day)
 }
