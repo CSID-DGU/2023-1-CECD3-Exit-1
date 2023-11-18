0cf5e9bf9b6bbc58ba2aa7b0956b9d4973c11a94
Dani43 <eldms0406@naver.com>
2023-11-18 18:26:00 +0900

    [feat] #75 약서랍 상세 조회 응답 DTO 추가

---
+++ app/src/main/java/com/cecd/exitmed/data/model/response/ResponseDrawerDetail.kt
@@ -0,0 +1,25 @@
+package com.cecd.exitmed.data.model.response
+
+import com.cecd.exitmed.domain.type.DrawerDetail
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseDrawerDetail(
+    val alarm: String,
+    val comment: String,
+    val dosage: String,
+    val finalDate: String,
+    val imageLink: String,
+    val pillName: String
+)
+
+fun ResponseDrawerDetail.toDrawerDetail(): DrawerDetail {
+    return DrawerDetail(
+        this.alarm,
+        this.comment,
+        this.dosage,
+        this.finalDate,
+        this.imageLink,
+        this.pillName
+    )
+}
