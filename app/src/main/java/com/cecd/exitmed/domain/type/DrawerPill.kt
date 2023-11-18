6a7a1f0dbbe52cc9d4679f74101d3bc3720a8afa
Dani43 <eldms0406@naver.com>
2023-11-18 18:28:10 +0900

    [feat] #75 약서랍 상세 뷰 구현

--- app/src/main/java/com/cecd/exitmed/domain/type/DrawerPill.kt
+++
@@ -1,8 +0,0 @@
-package com.cecd.exitmed.domain.type
-
-data class DrawerPill(
-    val pillId: Int,
-    val title: String,
-    val category: String,
-    val isAlarm: Boolean
-)
--- app/src/main/java/com/cecd/exitmed/presentation/pillDrawerDetail/PillDrawerDetailInfoFragment.kt
+++ app/src/main/java/com/cecd/exitmed/presentation/pillDrawerDetail/PillDrawerDetailInfoFragment.kt
@@ -2,25 +2,36 @@ package com.cecd.exitmed.presentation.pillDrawerDetail
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.cecd.exitmed.R
 import com.cecd.exitmed.databinding.FragmentPillDrawerDetailInfoBinding
 import com.cecd.exitmed.util.binding.BindingFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class PillDrawerDetailInfoFragment :
     BindingFragment<FragmentPillDrawerDetailInfoBinding>(R.layout.fragment_pill_drawer_detail_info) {
-    private val viewModel: PillDrawerDetailViewModel by viewModels()
+    private val viewModel: PillDrawerDetailViewModel by activityViewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initLayout()
+        collectData()
     }
 
     private fun initLayout() {
         val adapter = PillDrawerDetailDoseTimeAdapter()
         binding.rvPillDrawerDetailTime.adapter = adapter
         adapter.setDoseTimeList(viewModel.doseTimeMockList)
+    }
+
+    private fun collectData() {
+        viewModel.drawerPillDetail.flowWithLifecycle(lifecycle).onEach { drawerPillDetail->
+            binding.tvPillDrawerDetailLastDoseContent.text = drawerPillDetail?.finalDoseDate
+        }.launchIn(lifecycleScope)
     }
 }
