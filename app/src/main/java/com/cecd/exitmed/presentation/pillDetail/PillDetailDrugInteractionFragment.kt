2a1c71b606fd71e58dbe282be9370d84eb944bc5
Dani43 <eldms0406@naver.com>
2023-12-05 13:16:44 +0900

    [feat] #84 약 디테일 뷰 중복 투약 경고 구현

--- app/src/main/java/com/cecd/exitmed/presentation/pillDetail/PillDetailDrugInteractionFragment.kt
+++ app/src/main/java/com/cecd/exitmed/presentation/pillDetail/PillDetailDrugInteractionFragment.kt
@@ -1,16 +1,38 @@
 package com.cecd.exitmed.presentation.pillDetail
 
 import android.os.Bundle
+import android.text.Html
 import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.cecd.exitmed.R
 import com.cecd.exitmed.databinding.FragmentPillDetailDrugInteractionBinding
 import com.cecd.exitmed.util.binding.BindingFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class PillDetailDrugInteractionFragment :
     BindingFragment<FragmentPillDetailDrugInteractionBinding>(R.layout.fragment_pill_detail_drug_interaction) {
+    private val pillDetailViewModel: PillDetailViewModel by activityViewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        collectData()
+    }
+
+    private fun collectData() {
+        pillDetailViewModel.duplicatedPills.flowWithLifecycle(lifecycle)
+            .onEach { duplicatedPillList ->
+                val duplicatedPills = duplicatedPillList.joinToString(separator = ", ")
+                val content = getString(
+                    R.string.pill_detail_drug_interaction_content,
+                    Html.fromHtml(duplicatedPills, Html.FROM_HTML_MODE_LEGACY)
+                )
+                binding.tvPillDetailDrugInteractionContent.text =
+                    Html.fromHtml(content, Html.FROM_HTML_MODE_LEGACY)
+            }.launchIn(lifecycleScope)
     }
 }
