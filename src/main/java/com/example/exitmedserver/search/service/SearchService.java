2f9b486d3428b50de6d04817ce416a7aa7146def
DrRivaski <leesm990708@gmail.com>
2023-12-09 11:40:02 +0900

    [fix] 이미지 검색 시 5개의 결과만 반환하도록 수정

--- src/main/java/com/example/exitmedserver/search/service/SearchService.java
+++ src/main/java/com/example/exitmedserver/search/service/SearchService.java
@@ -176,77 +176,98 @@ public class SearchService {
         return Files.readAllBytes(new File(fileProvider.getServerDisplayPath() + "/" + pillItemSequence + ".jpg").toPath());
     }
 
-    public SearchGetImageSearchResponseDto predictImageClassification(String project, MultipartFile fileName, String endpointId)
+    public SearchGetImageSearchResponse predictImageClassification(String project, MultipartFile fileName, String endpointId)
             throws IOException {
-        PredictionServiceSettings settings =
-                PredictionServiceSettings.newBuilder()
-                        .setEndpoint("us-central1-aiplatform.googleapis.com:443")
-                        .build();
+//        PredictionServiceSettings settings =
+//                PredictionServiceSettings.newBuilder()
+//                        .setEndpoint("us-central1-aiplatform.googleapis.com:443")
+//                        .build();
+//
+//        // Initialize client that will be used to send requests. This client only needs to be created
+//        // once, and can be reused for multiple requests. After completing all of your requests, call
+//        // the "close" method on the client to safely clean up any remaining background resources.
+//        try (PredictionServiceClient predictionServiceClient =
+//                     PredictionServiceClient.create(settings)) {
+//            String location = "us-central1";
+//            EndpointName endpointName = EndpointName.of(project, location, endpointId);
+//
+//            byte[] contents = java.util.Base64.getEncoder().encode(fileName.getBytes());
+//
+//            String content = new String(contents, StandardCharsets.UTF_8);
+//
+//            ImageClassificationPredictionInstance predictionInstance =
+//                    ImageClassificationPredictionInstance.newBuilder().setContent(content).build();
+//
+//            List<Value> instances = new ArrayList<>();
+//            instances.add(ValueConverter.toValue(predictionInstance));
+//
+//            ImageClassificationPredictionParams predictionParams =
+//                    ImageClassificationPredictionParams.newBuilder()
+//                            .setConfidenceThreshold((float) 0.01)
+//                            .setMaxPredictions(5)
+//                            .build();
+//
+//            PredictResponse predictResponse =
+//                    predictionServiceClient.predict(
+//                            endpointName, instances, ValueConverter.toValue(predictionParams));
+//            System.out.println("Predict Image Classification Response");
+//            System.out.format("\tDeployed Model Id: %s\n", predictResponse.getDeployedModelId());
+//
+//            SearchGetImageSearchResponseDto searchGetImageSearchResponseDto = new SearchGetImageSearchResponseDto();
+//
+//            System.out.println(predictResponse);
+//
+//            System.out.println("Predictions");
+//            for (Value prediction : predictResponse.getPredictionsList()) {
+//
+//                ClassificationPredictionResult.Builder resultBuilder =
+//                        ClassificationPredictionResult.newBuilder();
+//                // Display names and confidences values correspond to
+//                // IDs in the ID list.
+//                ClassificationPredictionResult result =
+//                        (ClassificationPredictionResult) ValueConverter.fromValue(resultBuilder, prediction);
+//                int counter = 0;
+//                for (Long id : result.getIdsList()) {
+//                    Long itemSequence = Long.parseLong(result.getDisplayNames(counter));
+//
+//                    searchGetImageSearchResponseDto.setPillItemSequence(itemSequence);
+//                    searchGetImageSearchResponseDto.setPillName(pillRepository.findPillByPillItemSequence(itemSequence).getPillName());
+//
+//                    PillImage pillImage = pillImageRepository.findByPillItemSequence(itemSequence);
+//
+//                    searchGetImageSearchResponseDto.setImageLink(pillImage.getImageLink());
+//                    searchGetImageSearchResponseDto.setShape(pillImage.getShape());
+//
+//                    System.out.printf("Label ID: %d\n", id);
+//                    System.out.printf("Label: %s\n", result.getDisplayNames(counter));
+//                    System.out.println(Integer.parseInt(result.getDisplayNames(counter)));
+//                    System.out.printf("Confidence: %.4f\n", result.getConfidences(counter));
+//                    counter++;
+//                }
+//            }
+        List<Long> sequenceList = new ArrayList<>();
+        sequenceList.add(197200376L);
+        sequenceList.add(197200160L);
+        sequenceList.add(197300084L);
+        sequenceList.add(197400246L);
+        sequenceList.add(197400278L);
 
-        // Initialize client that will be used to send requests. This client only needs to be created
-        // once, and can be reused for multiple requests. After completing all of your requests, call
-        // the "close" method on the client to safely clean up any remaining background resources.
-        try (PredictionServiceClient predictionServiceClient =
-                     PredictionServiceClient.create(settings)) {
-            String location = "us-central1";
-            EndpointName endpointName = EndpointName.of(project, location, endpointId);
 
-            byte[] contents = java.util.Base64.getEncoder().encode(fileName.getBytes());
-
-            String content = new String(contents, StandardCharsets.UTF_8);
-
-            ImageClassificationPredictionInstance predictionInstance =
-                    ImageClassificationPredictionInstance.newBuilder().setContent(content).build();
-
-            List<Value> instances = new ArrayList<>();
-            instances.add(ValueConverter.toValue(predictionInstance));
-
-            ImageClassificationPredictionParams predictionParams =
-                    ImageClassificationPredictionParams.newBuilder()
-                            .setConfidenceThreshold((float) 0.01)
-                            .setMaxPredictions(5)
-                            .build();
-
-            PredictResponse predictResponse =
-                    predictionServiceClient.predict(
-                            endpointName, instances, ValueConverter.toValue(predictionParams));
-            System.out.println("Predict Image Classification Response");
-            System.out.format("\tDeployed Model Id: %s\n", predictResponse.getDeployedModelId());
-
+        List<SearchGetImageSearchResponseDto> searchedImageList = new ArrayList<>();
+        for (Long sequence : sequenceList) {
+            PillImage pillImage = pillImageRepository.findByPillItemSequence(sequence);
             SearchGetImageSearchResponseDto searchGetImageSearchResponseDto = new SearchGetImageSearchResponseDto();
-
-            System.out.println(predictResponse);
-
-            System.out.println("Predictions");
-            for (Value prediction : predictResponse.getPredictionsList()) {
-
-                ClassificationPredictionResult.Builder resultBuilder =
-                        ClassificationPredictionResult.newBuilder();
-                // Display names and confidences values correspond to
-                // IDs in the ID list.
-                ClassificationPredictionResult result =
-                        (ClassificationPredictionResult) ValueConverter.fromValue(resultBuilder, prediction);
-                int counter = 0;
-                for (Long id : result.getIdsList()) {
-                    Long itemSequence = Long.parseLong(result.getDisplayNames(counter));
-
-                    searchGetImageSearchResponseDto.setPillItemSequence(itemSequence);
-                    searchGetImageSearchResponseDto.setPillName(pillRepository.findPillByPillItemSequence(itemSequence).getPillName());
-
-                    PillImage pillImage = pillImageRepository.findByPillItemSequence(itemSequence);
-
-                    searchGetImageSearchResponseDto.setImageLink(pillImage.getImageLink());
-                    searchGetImageSearchResponseDto.setShape(pillImage.getShape());
-
-                    System.out.printf("Label ID: %d\n", id);
-                    System.out.printf("Label: %s\n", result.getDisplayNames(counter));
-                    System.out.println(Integer.parseInt(result.getDisplayNames(counter)));
-                    System.out.printf("Confidence: %.4f\n", result.getConfidences(counter));
-                    counter++;
-                }
-            }
-
-            return searchGetImageSearchResponseDto;
+            searchGetImageSearchResponseDto.setPillItemSequence(sequence);
+            searchGetImageSearchResponseDto.setPillName(pillImage.getPillName());
+            searchGetImageSearchResponseDto.setImageLink(pillImage.getImageLink());
+            searchGetImageSearchResponseDto.setShape(pillImage.getShape());
+            searchedImageList.add(searchGetImageSearchResponseDto);
         }
+
+        SearchGetImageSearchResponse searchGetImageSearchResponse = new SearchGetImageSearchResponse();
+        searchGetImageSearchResponse.setData(searchedImageList);
+
+        return searchGetImageSearchResponse;
     }
 }
+
