763114b49c92f752b894829f38bf1c4605dda523
Dani43 <eldms0406@naver.com>
2023-12-11 03:10:42 +0900

    [feat] #88 이미지 검색 top1 accuracy 결과뷰 구현

--- app/src/main/java/com/cecd/exitmed/presentation/imageSearch/ImageSearchViewModel.kt
+++ app/src/main/java/com/cecd/exitmed/presentation/imageSearch/ImageSearchViewModel.kt
@@ -1,11 +1,11 @@
 package com.cecd.exitmed.presentation.imageSearch
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.cecd.exitmed.data.model.response.ResponseImageSearch
 import com.cecd.exitmed.domain.repository.ImageSearchRepository
+import com.cecd.exitmed.domain.type.ImageSearchInfo
 import com.cecd.exitmed.util.ContentUriRequestBody
+import com.cecd.exitmed.util.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -19,24 +19,28 @@ class ImageSearchViewModel @Inject constructor(
 ) : ViewModel() {
     private val _image = MutableStateFlow<ContentUriRequestBody?>(null)
     val image get() = _image.asStateFlow()
-    private val _topAccuracyImageInfo = MutableStateFlow<ResponseImageSearch?>(null)
-    val topAccuracyImageInfo get() = _topAccuracyImageInfo.asStateFlow()
+    private val _imageSearchState =
+        MutableStateFlow<UiState<List<ImageSearchInfo>>>(UiState.Loading)
+    val imageSearchState get() = _imageSearchState.asStateFlow()
+    private val _imageSearchInfoLists = MutableStateFlow<List<ImageSearchInfo>>(listOf())
+    val imageSearchInfoLists get() = _imageSearchInfoLists.asStateFlow()
 
     fun setRequestBody(requestBody: ContentUriRequestBody) {
         _image.value = requestBody
     }
 
     fun searchImagePill() {
-        if (image.value != null) {
-            viewModelScope.launch {
-                image.value?.toFormData()?.let {
-                    imageSearchRepository.searchImagePill(it)
-                        .onSuccess { imageInfo ->
-                            _topAccuracyImageInfo.value = imageInfo
-                        }.onFailure { throwable ->
-                            Timber.e(throwable.message)
-                        }
-                }
+        viewModelScope.launch {
+            image.value?.toFormData()?.let { image ->
+                imageSearchRepository.searchImagePill(image)
+                    .onSuccess {
+                        _imageSearchInfoLists.value = it
+                        _imageSearchState.value = UiState.Success(it)
+                    }
+                    .onFailure { throwable ->
+                        _imageSearchState.value = UiState.Error(throwable.message)
+                        Timber.e(throwable.message)
+                    }
             }
         }
     }
